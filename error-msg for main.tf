3710bfae3d8a8ed143772aad9f62e6b854ec8325
Suraj Waghamare <surajnm.waghmare@gmail.com>
2025-06-06 20:46:46 +0530

    Create error-msg for main.tf

---
+++ error-msg for main.tf
@@ -0,0 +1,8 @@
+now got this error
+
+╷
+│ Error: Failed to create deployment: Unauthorized
+│
+│   with kubernetes_deployment.app,
+│   on main.tf line 77, in resource "kubernetes_deployment" "app":
+│   77: resource "kubernetes_deployment" "app" {
