d6e50e5a8b6018c2527c2f4d29d68ff16c8b96d5
Suraj Waghamare <surajnm.waghmare@gmail.com>
2025-06-07 14:07:04 +0530

    Create variables.tf

---
+++ terraform/ECS/variables.tf
@@ -0,0 +1,26 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources in"
+  default     = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "container_image" {
+  description = "Docker container image to deploy"
+  default     = "suraj838098/simpletimeservice:latest"
+}
