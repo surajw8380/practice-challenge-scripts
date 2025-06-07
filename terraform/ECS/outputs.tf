417736b6fd76bca85ff9d3dc0495494c52d33fb6
Suraj Waghamare <surajnm.waghmare@gmail.com>
2025-06-07 16:16:01 +0530

    Update outputs.tf

--- terraform/ECS/outputs.tf
+++ terraform/ECS/outputs.tf
@@ -1,4 +1,4 @@
 output "load_balancer_dns" {
   description = "Public DNS of the Application Load Balancer"
-  value       = aws_lb.this.dns_name
+  value       = "http://${aws_lb.this.dns_name}"
 }
